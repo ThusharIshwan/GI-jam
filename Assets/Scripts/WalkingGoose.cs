f079234ce32553849b85730471b32d3a4ff6dcf8
00vvv <43686861+00vvv@users.noreply.github.com>
2018-09-28 20:17:04 -0400

    Asset - GooseUnity

---
+++ Assets/Scripts/WalkingGoose.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingGoose : MonoBehaviour {
+    private int currentsprite = 0;
+    private SpriteRenderer sr;
+    private Transform transform;
+    public Sprite[] goose = new Sprite[3];
+    private float gst = 0;
+    public float speed;
+    // Use this for initialization
+    void Start () { 
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        transform = gameObject.GetComponent<Transform>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gst > (1 / speed))
+        {
+            currentsprite = (currentsprite + 1) % 3;
+            sr.sprite = goose[currentsprite];
+            gst = 0;
+            transform.position = new Vector3(transform.position.x + (speed / 25),
+                                  transform.position.y, 0);
+        }
+        else { gst += Time.deltaTime; }
+
+    }
+    
+}
